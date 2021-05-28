d59ce008a4abf44de93e9bbf981d491eafb514f8
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2021-05-28 20:10:38 +0700

    To main

--- Assets/Script/TriggerEvents.cs
+++ Assets/Script/TriggerEvents.cs
@@ -20,4 +20,39 @@ public class TriggerEvents : MonoBehaviour
             return;
         TriggerExiting?.Invoke();
     }
+
+    public GameObject RotatedObject;
+    [Range(0.25f, 5)]
+    public float RotateDuration = 1f;
+    public void RotateObject(float Angle)
+    {
+        //RotatedObject.transform.Rotate(new Vector3(0, 0, Angle));
+        StartCoroutine(SlowlyRotateTo(RotatedObject.transform, Angle));
+    }
+
+    public IEnumerator SlowlyRotateTo(Transform wantedObject, float angle)
+    {
+        float begin = 0;
+        if (angle > 1 || angle < -1)
+        {
+            Quaternion origin = wantedObject.rotation;
+            while (begin < RotateDuration)
+            {
+                wantedObject.rotation = Quaternion.Lerp(origin, Quaternion.Euler(0, 0, angle), begin / RotateDuration);
+                begin += Time.smoothDeltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        else
+        {
+            Quaternion origin = wantedObject.rotation;
+            while (begin < RotateDuration)
+            {
+                wantedObject.rotation = Quaternion.Lerp(origin, Quaternion.Euler(0, 0, 0), begin / RotateDuration);
+                begin += Time.smoothDeltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        yield return new WaitForEndOfFrame();
+    }
 }
