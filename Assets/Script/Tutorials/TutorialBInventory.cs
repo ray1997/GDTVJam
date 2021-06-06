b0dae051a1b80dfaa4bef63a4cca55317dcbb637
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2021-06-06 18:36:22 +0700

    Fix elevator button quest

---
+++ Assets/Script/Tutorials/TutorialBInventory.cs
@@ -0,0 +1,56 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class TutorialBInventory : MonoBehaviour
+{
+    public TMP_Text DisplayText;
+    public PlayerState Player1Inv;
+
+    private void OnEnable()
+    {
+        ObjectiveInfo.OnObjectiveFinished += WaitForQuestToSelfDestruct;
+    }
+
+    private void WaitForQuestToSelfDestruct(ObjectiveInfo sender, ObjectiveFinishedEventArgs args)
+    {
+        if (args.FinishedQuest == Quests.P1FindFuse)
+            DisplayText.text = "You found a fuse!\r\nPress [Q] to open inventory\r\nThen use fuse it to replace the broken one";
+        else if (args.FinishedQuest == Quests.P1ReplaceFuse)
+            DisplayText.text = "Now active the Circuit!";
+        else if (args.FinishedQuest == Quests.P1TurnOnCircuit)
+        {
+            DisplayText.text = "Now press [Space] to switch to your friend!";
+            Invoke(nameof(DelayDisable), 5);
+        }
+    }
+
+    public void DelayDisable() => TutorialRoot.SetActive(false);
+    public GameObject TutorialRoot;
+
+    private void OnDisable()
+    {
+        ObjectiveInfo.OnObjectiveFinished -= WaitForQuestToSelfDestruct;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check inventory for context
+        var fuse = Player1Inv.PlayerInventory.FirstOrDefault(i => i.Name == "Fuse");
+        if (fuse is null)
+            DisplayText.text = "It's appear that the fuse is busted.\r\nGo find a replacement fuse";
+        else
+            DisplayText.text = "You found a fuse!\r\nPress [Q] to open inventory\r\nThen use fuse it to replace the broken one";
+        if (other.tag == "Player")
+            DisplayText.DOFade(1, 1);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            DisplayText.DOFade(0, 1);
+    }
+}
